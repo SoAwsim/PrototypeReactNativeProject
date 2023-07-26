fa8ab1b80dd8999c4214ada22321905435628c91
iremsagsoz <iremtugbasagsoz@gmail.com>
2023-07-26 22:18:43 +0300

    comment

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import NavigationRoot from './src/NavigationRoot';
 
+//comment<3
 export default function App() {
   return (
 	<SafeAreaProvider>
