165f4a05fa66deb96c83227f35f7393432fb1862
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2023-08-08 22:48:03 +0300

    Add custom drawer layout

--- src/navigation/HomeDrawerNavigation.js
+++ src/navigation/HomeDrawerNavigation.js
@@ -1,15 +1,50 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { Text, View, useWindowDimensions } from "react-native";
+import { createDrawerNavigator, DrawerContentScrollView } from "@react-navigation/drawer";
+import { useContext } from "react";
+import { View, useWindowDimensions } from "react-native";
+import { Drawer } from "react-native-paper";
 import DummyHome from "../screens/dummyhome/DummyHome";
 import SettingsScreen from "../screens/settings/SettingsScreen";
+import { AuthContext } from "../context/AuthContext";
 
-const Drawer = createDrawerNavigator();
+const DrawerNavigation = createDrawerNavigator();
 
-function HomeDrawerContent() {
+function HomeDrawerContent(props) {
+    const state = props.navigation.getState();
+    const drawerIcons = [
+        {focus: 'home', unfocus: 'home-outline'},
+        {focus: 'cog', unfocus: 'cog-outline'}
+    ]
+
+    const { signOut } = useContext(AuthContext);
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Drawer</Text>
-        </View>
+        <DrawerContentScrollView {...props}>
+            <View style={{ flex: 1 }}>
+                <Drawer.Section>
+                    {state.routes.map((route, i) => {
+                        const focused = i === state.index;
+
+                        return (
+                            <Drawer.Item
+                                key={route.key}
+                                label={route.name}
+                                icon={focused ? drawerIcons[i].focus : drawerIcons[i].unfocus}
+                                active={focused}
+                                onPress={() => props.navigation.navigate(route.name)}
+                            />
+                        )
+                    })}
+                </Drawer.Section>
+                <Drawer.Section>
+                    <Drawer.Item
+                        label="Sign Out"
+                        icon='logout'
+                        active={false}
+                        onPress={() => signOut()}
+                    />
+                </Drawer.Section>
+            </View>
+        </DrawerContentScrollView>
     )
 }
 
@@ -17,22 +52,23 @@ export const HomeNavigator = () => {
     const windowsWidth = useWindowDimensions().width;
 
     return (
-        <Drawer.Navigator 
+        <DrawerNavigation.Navigator 
             initialRouteName="Home"
             screenOptions={{
                 headerShown: false,
                 swipeEdgeWidth: windowsWidth,
             }}
+            drawerContent={HomeDrawerContent}
         >
-            <Drawer.Screen
+            <DrawerNavigation.Screen
                 name="Home"
                 component={DummyHome}
             />
-            <Drawer.Screen
+            <DrawerNavigation.Screen
                 name="Settings"
                 component={SettingsScreen}
                 options={{ swipeEnabled: false }}
             />
-        </Drawer.Navigator>
+        </DrawerNavigation.Navigator>
     )
 }
