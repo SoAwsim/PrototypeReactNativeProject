9986e9108a6f8cff71a890b2be02df507fdc17ff
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2023-08-16 17:54:03 +0300

    Fixed drawer bug

--- src/app/home/home-drawer/home.js
+++ src/app/home/home-drawer/home.js
@@ -4,17 +4,19 @@ import { Appbar, Text } from "react-native-paper";
 import { View, StyleSheet } from "react-native";
 import i18n from "../../../localization/i18n";
 import { LocaleContext } from "../../../context/AppContext";
+import { useNavigation } from "expo-router";
 
 export default function Home() {
     const insets = useSafeAreaInsets();
     const style = styles(insets);
 
     const { displayLang } = useContext(LocaleContext);
+    const navigation = useNavigation();
 
     return (
         <>
             <Appbar.Header>
-                <Appbar.Action icon="menu" onPress={() => {}} />
+                <Appbar.Action icon="menu" onPress={() => navigation.openDrawer()} />
             </Appbar.Header>
             <View style={style.SafeAreaFlex}>
                 <Text>{i18n.t('dummyHomeScreen.dummyText', { locale: displayLang })}</Text>
